3af7df4c828077365ca856af5c3cb4fcf6814f21
gatesyp <ig11@zips.uakron.edu>
2016-04-19 16:26:53 -0400

    added path variables

--- setupPi.sh
+++ setupPi.sh
@@ -3,11 +3,11 @@ $!/bin/bash
 
 sudo apt-get install pip
 sudo pip install speedtest-cli
+sudo apt-get install arp-scan
 # edit path variable 
 CONFIG=/home/user/config.inc
 API=/home/user/api.txt
 
 sed -e "$API" >> $CONFIG
 
-sudo apt-get install arp-scan
-
+*/15 * * * * pi /home/pi/dayindata/<arp/speedtestscripts>.sh
