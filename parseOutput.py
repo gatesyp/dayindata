ca6ed5cf4a7b6ba518595f6ee52483f83d3bbddd
gatesyp <ig11@zips.uakron.edu>
2016-04-20 01:19:58 +0000

    added emersons code

--- parseOutput.py
+++ parseOutput.py
@@ -3,7 +3,14 @@ while True:
     text = f.readline
     if 'timestamp format' in text: # add in timestamp string here to look for times
         print text
-    elif 'emersons useful data' in text: # add in the strings for our useful txt
+    elif 'Download: ' in text: 
+        text = text.replace(test[10:15])
+        print text
+    elif 'Upload: ' in text: 
+        text = text.replace(test[8:13])
+        print text
+    elif 'Hosted by: ' in text: 
+        text = text.replace(test[:]) # look up how to get last 5 characters of string
         print text
     elif 'stevens useful data' in text: # add in the strings for our useful txt
         print text
